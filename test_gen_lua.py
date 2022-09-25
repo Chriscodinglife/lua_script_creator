62dad93ec6683edd44e2e2341acaf491d5f6ec10
Chriscodinglife <chrisdotorellana@gmail.com>
2022-09-24 21:46:41 -0400

    lowered imports below heredoc

--- test_gen_lua.py
+++ test_gen_lua.py
@@ -1,6 +1,3 @@
-import unittest
-import gen_lua as gl
-
 '''
  ______   
 /\  ___\  
@@ -13,9 +10,15 @@ A test script for ensuring the gen_lua.py script runs properly
 
 '''
 
+import unittest
+import gen_lua as gl
+
+
 class Test_gen_lua(unittest.TestCase):
 
-    '''The main test class to verify gen_lua is working properly'''
+    '''
+    The main test class to verify gen_lua is working properly
+    '''
 
     def test_read_lua(self):
 
@@ -23,6 +26,6 @@ class Test_gen_lua(unittest.TestCase):
         self.assertEqual(gl.get_lua(), True)
 
 
-if __name__ == '__main__'
+if __name__ == '__main__':
     
     unittest.main()
