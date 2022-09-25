18ecb82ced3f4f091ff6181b9f3258130a883d52
Chriscodinglife <chrisdotorellana@gmail.com>
2022-09-24 21:26:15 -0400

    added for commit

---
+++ gen_lua.py
@@ -0,0 +1,20 @@
+import os
+
+'''
+
+ ______   
+/\  ___\  
+\ \ \____ 
+ \ \_____\
+  \/_____/
+                                                   
+Generate OBS Lua
+
+This is intended to output a lua script that can be used with OBS to create a user interface.
+Ideally for creating scenes and importing items.
+
+I am aware of some python to lua translators but my use case is somewhat specific. C'est la vie.
+
+'''
+
+
