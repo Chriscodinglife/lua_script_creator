e118434c9d183c3b6c029ade750ec756a811cdcb
Chriscodinglife <chrisdotorellana@gmail.com>
2022-09-24 21:38:43 -0400

    created basic structure for test

---
+++ test_gen_lua.py
@@ -0,0 +1,28 @@
+import unittest
+import gen_lua as gl
+
+'''
+ ______   
+/\  ___\  
+\ \ \____ 
+ \ \_____\
+  \/_____/
+                 
+
+A test script for ensuring the gen_lua.py script runs properly
+
+'''
+
+class Test_gen_lua(unittest.TestCase):
+
+    '''The main test class to verify gen_lua is working properly'''
+
+    def test_read_lua(self):
+
+        '''Ensure lua template can be read'''
+        self.assertEqual(gl.get_lua(), True)
+
+
+if __name__ == '__main__'
+    
+    unittest.main()
