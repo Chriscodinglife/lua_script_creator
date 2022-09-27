bd6257088aaa72fd4f492c934088cfa4c4e142d2
Chriscodinglife <chrisdotorellana@gmail.com>
2022-09-26 22:32:35 -0400

    Added comments and test functionality for replacement text

--- gen_lua.py
+++ gen_lua.py
@@ -25,10 +25,15 @@ def main():
   General Run for gen_lua
   '''
 
+  # Get the location of the template.lua file
   current_dir = os.path.dirname(__file__)
   lua_template = os.path.join(current_dir, "template.lua")
 
+  # Initiate luator
   lua = luator(lua_template)
+
+  # Test replace a text
+  lua.replace_text("<package_name>", "This cool title here")
 
 
 if __name__ == '__main__':
