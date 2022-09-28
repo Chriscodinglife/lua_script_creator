eab3e775acc79b0d1c7789e350a946612d08a726
Chriscodinglife <chrisdotorellana@gmail.com>
2022-09-27 21:09:27 -0400

    corrected order of tests

--- test_luator.py
+++ test_luator.py
@@ -22,7 +22,12 @@ class Test_luator(unittest.TestCase):
     The main test class to verify Luator is working properly
     '''
     
-    def test_output_file(self):
+    def test_clean_up(self):
+        '''Pass if luator can remove the temp file and there is no longer any temp file'''
+        self.assertTrue(lua.clean_up())
+
+
+    def test_autput_file(self):
         '''Pass if luator can export a file to a given directory'''
         path = "~/Desktop/"
         folder_path = os.path.expanduser(path)
