d291bbe9f98831b16b1e781e03423504c76a0744
Chriscodinglife <chrisdotorellana@gmail.com>
2022-09-28 19:35:18 -0400

    Added test funcs for fastapi

--- gen_lua.py
+++ gen_lua.py
@@ -12,7 +12,21 @@ Generate OBS Lua Fast API Server
 
 '''
 
+from typing import Union
+from fastapi import FastAPI
 from luator import luator
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+  return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+  return {"item_id": item_id, "q": q}
 
 
 def main():
