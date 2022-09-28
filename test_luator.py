1a0f082595b84555e785312c83e421f0c16529d3
Chriscodinglife <chrisdotorellana@gmail.com>
2022-09-27 20:46:07 -0400

    added the test_output_file func

--- test_luator.py
+++ test_luator.py
@@ -10,21 +10,25 @@ A test script for ensuring the gen_lua.py script runs properly
 
 '''
 
+import os
 import unittest
-from gen_lua import genlua
+from luator import luator
 
+lua = luator()
 
-class Test_gen_lua(unittest.TestCase):
+class Test_luator(unittest.TestCase):
 
     '''
-    The main test class to verify gen_lua is working properly
+    The main test class to verify Luator is working properly
     '''
-    gl = genlua()
-
-    def test_read_lua(self):
-
-        '''Ensure lua template can be read'''
-        self.assertEqual(self.gl.get_lua(), "<class 'str'>")
+    
+    def test_output_file(self):
+        '''Pass if luator can export a file to a given directory'''
+        path = "~/Desktop/test_folder"
+        folder_path = os.path.expanduser(path)
+        full_file_path = os.path.join(folder_path,"test.lua")
+        message = "File could not be exported"
+        self.assertTrue(lua.export_lua(full_file_path), message)
 
 
 if __name__ == '__main__':
