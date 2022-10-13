de20cd7cdc663c5ee7566d8991873dbf873911f1
Chriscodinglife <corella4@pratt.edu>
2022-10-12 22:19:43 -0400

    removed removecharacter func

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -7,16 +7,6 @@ class App extends Component {
         characters: [],
     }
 
-    removeCharacter = (index) => {
-        const { characters } = this.state
-
-        this.setState({
-            characters: characters.filter((character, i) => {
-                return i !== index
-            }),
-        })
-    }
-
     handleSubmit = (character) => {
         this.setState({ characters: [...this.state.characters, character] })
     }
