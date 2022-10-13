d15a36437bc2b948e989497aaf3d5d8d86010704
Chriscodinglife <corella4@pratt.edu>
2022-10-12 22:18:51 -0400

    removed table in app

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -27,7 +27,6 @@ class App extends Component {
 
         return (
             <div className="container">
-                <Table characterData={characters}  removeCharacter={this.removeCharacter} />
                 <Form handleSubmit={this.handleSubmit} />
             </div>
         )
