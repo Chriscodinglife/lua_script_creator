9382a42428a31da456e1d5d55465b8b995584667
Chriscodinglife <corella4@pratt.edu>
2022-10-15 16:02:23 -0400

    setup the image conversion endpoint

--- services/image_convert_backend/main.py
+++ services/image_convert_backend/main.py
@@ -12,7 +12,7 @@ Server to return a base64 encoded image string
 
 '''
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from image_converter import ImageConverter
 from fastapi import FastAPI, File, UploadFile
 
@@ -25,16 +25,14 @@ def ping():
   return {"response": "Hello there"}
 
 
-@app.post("/convert/")
+@app.post("/convert/", status_code=status.HTTP_201_CREATED)
 async def convert_image(file: UploadFile = File(...)):
   '''Return an image in base64 data'''
   image_converter = ImageConverter()
-  return {"response": file.filename}
-  # image_converter.create_image_file(file_name=fileb.filename, file=fileb.file)
-  # if image_converter.check_image_file(image_path=image_converter.image_path):
-  #   image = image_converter.resize_image()
-  #   image_base64_str = image_converter.convert_image_to_base64(image=image)
-  #   return {"image": image_base64_str}
-  # else:
-  #   return {"error": "File is not an image"}
-
+  image_converter.create_image_file(file_name=file.filename, file=file.file)
+  if image_converter.check_image_file(image_path=image_converter.image_path):
+    image = image_converter.resize_image()
+    image_base64_str = image_converter.convert_image_to_base64(image=image)
+    return {"image": image_base64_str}
+  else:
+    return {"error": "File is not an image"}
