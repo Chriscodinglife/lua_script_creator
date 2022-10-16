e9722bcfc779b6a53924e34b29364a970a1af966
Chriscodinglife <corella4@pratt.edu>
2022-10-16 12:16:40 -0400

    reorgnized input orders

--- services/frontend/src/Form.js
+++ services/frontend/src/Form.js
@@ -2,6 +2,30 @@ export default function Form(props) {
 
     return (
         <form onSubmit={props.submit}>
+            <label htmlFor='pkgName'>Package Name</label>
+            <input
+                type = "text"
+                name = "pkgName"
+                value = {props.values.pkgName}
+                onChange = {props.handleChange}></input>
+            <label htmlFor="storeName">Store Name</label>
+            <input
+                type = "text"
+                name = "storeName"
+                value = {props.values.storeName}
+                onChange = {props.handleChange}></input>
+            <label htmlFor='supportLink'>Support Link</label>
+            <input
+                type = "text"
+                name = "supportLink"
+                value = {props.values.supportLink}
+                onChange = {props.handleChange}></input>
+            <label htmlFor="manual">Manual Link</label>
+            <input
+                type = "text"
+                name = "manual"
+                value = {props.values.manual}
+                onChange = {props.handleChange}></input>
             <label htmlFor="logoImage">Logo Image</label>
             <input
                 type = "file"
@@ -12,30 +36,6 @@ export default function Form(props) {
                 type = "file"
                 name = "productImage"
                 onChange = {props.handleProduct}></input>
-            <label htmlFor='pkgName'>Package Name</label>
-            <input
-                type = "text"
-                name = "pkgName"
-                value = {props.values.pkgName}
-                onChange = {props.handleChange}></input>
-            <label htmlFor='supportLink'>Support Link</label>
-            <input
-                type = "text"
-                name = "supportLink"
-                value = {props.values.supportLink}
-                onChange = {props.handleChange}></input>
-            <label htmlFor="storeName">Store Name</label>
-            <input
-                type = "text"
-                name = "storeName"
-                value = {props.values.storeName}
-                onChange = {props.handleChange}></input>
-            <label htmlFor="manual">Manual Link</label>
-            <input
-                type = "text"
-                name = "manual"
-                value = {props.values.manual}
-                onChange = {props.handleChange}></input>
             <button type="submit">Download OBS File</button>
         </form>
     );
