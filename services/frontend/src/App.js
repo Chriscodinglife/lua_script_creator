9b10c5907874a8bf428f527d3bddf72ba1d96eb8
Chriscodinglife <corella4@pratt.edu>
2022-10-16 11:58:58 -0400

    Added logic for talking to image converter backend

--- services/frontend/src/App.js
+++ services/frontend/src/App.js
@@ -3,26 +3,70 @@ import { useState } from 'react';
 import Form from './Form';
 
 function App() {
+
+  const image_convert_backend = 'http://localhost:8080';
+
   const [allValues, setAllValues] = useState({
-    logoImage: '',
-    productImage: '',
+    logoData: '',
+    productData: '',
     pkgName: '',
     supportLink: '',
     storeName: '',
     manual: ''
   });
 
+  const [logoPath, setLogoPath] = useState('');
+  const [productPath, setProductPath] = useState('');
+
+  
+  const fetchImageData = (imagePath, setData) => {
+
+    const formData = new FormData();
+    formData.append('file', imagePath);
+    
+    fetch(image_convert_backend + "/convert", {
+      method: 'POST',
+      body: formData
+    })
+    .then(response => response.json())
+    .then(result => {
+      setAllValues({...allValues, [setData]: result['image']});
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });
+  };
+
+
+  const logoHandler = (e) => {
+    setLogoPath(e.target.files[0]);
+
+    fetchImageData(e.target.files[0], 'logoData');
+
+  };
+
+
+  const productHandler = (e) => {
+    setProductPath(e.target.files[0]);
+
+    fetchImageData(e.target.files[0], 'productData');
+
+  };
+
+
   const handleChange = (e) => {
     setAllValues({...allValues, [e.target.name]: e.target.value});
   };
 
-  const handleSubmit = () => {
-    console.log("yes")
-  }
+
+  const handleSubmit = (e) => {
+    const obsdata = allValues;
+    
+  };
 
   return (
     <div>
-      <Form values={allValues} handleChange={handleChange} submit={handleSubmit}></Form>
+      <Form values={allValues} handleChange={handleChange} submit={handleSubmit} handleLogo={logoHandler} handleProduct={productHandler}></Form>
     </div>
     );
 };
