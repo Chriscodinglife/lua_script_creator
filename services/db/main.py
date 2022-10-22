ada77b4972263321d281be98ac637dfab3f9af1b
Chriscodinglife <corella4@pratt.edu>
2022-10-22 19:33:01 -0400

    added a new add screens and add all screens func

--- services/db/main.py
+++ services/db/main.py
@@ -1,16 +1,12 @@
-from app.models import Project, ScreenNames, Screen, ProjectItems
+from app.models import Project
+from app.create_comps import CreateComps, ScreenTypes
 from fastapi import FastAPI, status, Form
-from tortoise.contrib.pydantic import pydantic_model_creator, pydantic_queryset_creator
+from tortoise.contrib.pydantic import pydantic_model_creator
 from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
 
 app = FastAPI()
 
 project_pydantic = pydantic_model_creator(Project)
-screen_pydantic = pydantic_model_creator(Screen)
-screen_pydantic_list = pydantic_queryset_creator(Screen)
-project_items_pydantic = pydantic_model_creator(ProjectItems)
-project_items_pydantic_list = pydantic_queryset_creator(ProjectItems)
-
 
 @app.get("/ping")
 async def ping():
@@ -18,13 +14,16 @@ async def ping():
 
 
 @app.post("/project/create/", status_code=status.HTTP_201_CREATED)
-async def create_project(name=Form(...), description=Form(...)):
-    project = await Project.create(name=name, description=description)
+async def create_project(project_name=Form(...), project_description=Form(...)):
+    ''' Create a new project '''
+    project = await Project.create(project_name=project_name, 
+                                    project_description=project_description)
     return await project_pydantic.from_tortoise_orm(project)
 
 
 @app.get("/projects/", status_code=status.HTTP_200_OK)
 async def get_projects():
+    ''' Get all projects '''
     list = await project_pydantic.from_queryset(Project.all())
     if len(list) == 0:
         return {'Response': "No Projects"}
@@ -32,32 +31,32 @@ async def get_projects():
         return list
 
 
-@app.post("/project/screens/add/", status_code=status.HTTP_202_ACCEPTED)
-async def add_screen(screen_names: ScreenNames, project_id: int):
+@app.post("/project/{project_id}/add_screen_comp/", status_code=status.HTTP_201_CREATED)
+async def add_screen_comp(project_id: int, screen_type: ScreenTypes):
+    ''' Add a screen comp to a project '''
     project = await Project.get(id=project_id)
-    screen_item = await Screen.create(screen_names=screen_names, project=project)
-    project_items = await ProjectItems.get_or_none(project=project)
-    if project_items == None:
-        project_items = await ProjectItems.create(project=project)
+    if not project:
+        raise HTTPNotFoundError
+    create_comps = CreateComps()
+    screen_comps = create_comps.add_screen_comp(project_comps=project.project_comps['Comps'], screen_type=screen_type)
 
-    await project_items.items.add(screen_item)
-    await project_items.save()
+    project.project_comps = screen_comps
+    await project.save()
+    return await project
 
-    return await screen_pydantic.from_tortoise_orm(screen_item)
 
-
-@app.get("/project/screens/", status_code=status.HTTP_202_ACCEPTED)
-async def get_project_screens(project_id: int):
+@app.post("/project/{project_id}/add_all_screens/", status_code=status.HTTP_201_CREATED)
+async def add_all_screens(project_id: int):
+    ''' Add all screen comps to a project '''
     project = await Project.get(id=project_id)
-    return await screen_pydantic_list.from_queryset(Screen.filter(project=project))
+    if not project:
+        raise HTTPNotFoundError
+    create_comps = CreateComps()
+    screen_comps = create_comps.add_all_screens(project_comps=project.project_comps['Comps'])
 
-
-@app.get("/project/items/", status_code=status.HTTP_202_ACCEPTED)
-async def get_project_items(project_id: int):
-    project = await Project.get(id=project_id)
-    project_items = await ProjectItems.get(project=project)
-    return await project_items.items.all()
-
+    project.project_comps = screen_comps
+    await project.save()
+    return await project
 
     
 register_tortoise(
