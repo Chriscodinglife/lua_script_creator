22d82ea198e5fcaedfd89b9c789eef8cf72f474c
Chriscodinglife <corella4@pratt.edu>
2022-10-26 23:05:01 -0400

    edited the projectlist

--- services/frontend-projectcreator/src/ProjectList.js
+++ services/frontend-projectcreator/src/ProjectList.js
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 const ProjectList = ({projects, title}) => {
 
     return ( 
@@ -5,9 +7,11 @@ const ProjectList = ({projects, title}) => {
             <h2>{title}</h2>
             {projects.map((project) => (
                 <div className="project-preview" key={project.id}>
-                    <h2>{project.project_name}</h2>
-                    <p>Status: {project.project_status}</p>
-                    <p>Edited on: {project.modified_time}</p>
+                    <Link to={`/project/${project.id}`}>
+                        <h2>{project.project_name}</h2>
+                        <p>Status: {project.project_status}</p>
+                        <p>Edited on: {project.modified_time}</p>
+                    </Link>
                 </div>
             ))}
         </div>
