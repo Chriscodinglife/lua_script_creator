18ce705f91a5d0af309a0253c9312b54f517f93e
Chriscodinglife <corella4@pratt.edu>
2022-10-29 23:13:59 -0400

    huh?

--- services/frontend-projectcreator/src/ProjectList.js
+++ services/frontend-projectcreator/src/ProjectList.js
@@ -2,18 +2,26 @@ import { Link } from "react-router-dom";
 
 const ProjectList = ({projects, title}) => {
 
+    const noProjects = projects.No_Projects;
+    let displayProjects = '';
+    if (noProjects) {
+        displayProjects = <p>No Projects Available</p>
+    } else {
+        displayProjects = projects.map((project) => (
+            <div className="project-preview" key={project.id}>
+                <Link to={`/project/${project.id}`}>
+                    <h2>{project.project_name}</h2>
+                    <p>Status: {project.project_status}</p>
+                    <p>Edited on: {project.modified_time}</p>
+                </Link>
+            </div>
+        ))
+    }
+
     return ( 
         <div className="project-list">
             <h2>{title}</h2>
-            {projects.map((project) => (
-                <div className="project-preview" key={project.id}>
-                    <Link to={`/project/${project.id}`}>
-                        <h2>{project.project_name}</h2>
-                        <p>Status: {project.project_status}</p>
-                        <p>Edited on: {project.modified_time}</p>
-                    </Link>
-                </div>
-            ))}
+            {displayProjects}
         </div>
      );
 }
