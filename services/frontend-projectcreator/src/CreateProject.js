1e10f6c8b2b45756131224345f2a46abd8157b8b
Chriscodinglife <corella4@pratt.edu>
2022-11-04 23:35:09 -0400

    added useeffect to get the project id

--- services/frontend-projectcreator/src/CreateProject.js
+++ services/frontend-projectcreator/src/CreateProject.js
@@ -1,5 +1,5 @@
-import { useState } from "react";
 import useFetch from "./useFetch";
+import { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
 
 const CreateProject = () => {
@@ -12,11 +12,12 @@ const CreateProject = () => {
     const [selectedScreen, setSelectedScreen] = useState('')
     const [screens, setScreens] = useState([]);
     const [isFormPending, setIsFormPending] = useState(false)
+    const [projectId, setProjectId] = useState(null)
     const navigate = useNavigate()
 
-
-    // Check if the screen type we are trying to add is already in current screens
+    
     const screenTypeFound = (add_screen_type) => screens.some(screen_type => {
+        // Check if the screen type we are trying to add is already in current screens
         if (screen_type === add_screen_type) {
             return true;
         }
@@ -27,16 +28,20 @@ const CreateProject = () => {
 
     const handleAddScreen = (e) => {
 
+        // Only add the screen if its not blank and its not in the current screens set
         if (e.target.value !== " " && !screenTypeFound(e.target.value)) {
 
             setScreens((current_screens) => [...current_screens, e.target.value]);
+            // Also set the current selected screen to the one we just added
             setSelectedScreen(e.target.value)
-        }
+        };
 
     };
 
 
     const addAllScreens = () => {
+
+        // Add all the screen types to the current screens set
         screen_types.ScreenTypes.map((screen) => {
             if (!screenTypeFound(screen)) {
                 setScreens((current_screens) => [...current_screens, screen]);
@@ -47,51 +52,93 @@ const CreateProject = () => {
 
 
     const handleRemoveScreen = (thisScreen) => {
+
+        // Remove the screen from the current screens set
         const newScreens = screens.filter(screen => screen !== thisScreen);
-        setScreens(newScreens)
-    }
+        setScreens(newScreens);
+    };
 
 
     const screensLength = () => {
+        // Return true or false depending on if there are any screens in the current screens set
         if (screens.length > 0) {
-            return true
+            return true;
         }
-        return false
-    }
+        return false;
+    };
 
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-
-        setIsFormPending(true);
-
+    const createProject = async () => {
+            
+        // Create the project
         const create_url = 'http://localhost:8000/project/create/'
-        fetch(create_url, { 
+        const response = await fetch(create_url, {
             method: 'POST',
             headers: { "Content-Type": "application/x-www-form-urlencoded" },
             body: new URLSearchParams({
                 'project_name': title,
                 'project_description': desc
             })
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw Error('Could not post data')
-            }
-            return response.json()
-        })
-        .then(data => {
-            const add_screens_url = "http://localhost:8000/project/" + data.id + "/add_screen_comp/"
+        });
+
+        if (!response.ok) {
+            throw Error('Could not post data');
+        }
+        const data = await response.json();
+        setProjectId(data.id);
+
+    };
+
+
+    const addScreenComps = () => {
+        // Add the screen components to the project
+
+        if (screensLength()) {
+            const add_screens_url = "http://localhost:8000/project/" + projectId + "/add_screen_comp/"
             fetch(add_screens_url, {
                 method: 'POST',
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify(screens)
             })
-            .then(() => {
-                setIsFormPending(false)
-                navigate('/project/' + data.id)
+            .then(response => {
+                if (!response.ok) {
+                    throw Error('Could not post data');
+                }
             });
-        }); 
+        };
+    };
+
+
+    useEffect(() => {
+        // When the project id is set, add the screen components and download the project folder
+        if (projectId) {
+            addScreenComps();
+            // downloadProjectFolder();
+            navigate('/project/' + projectId);
+            setIsFormPending(false);
+        }
+    }, [projectId]);
+
+
+    const handleSubmit = (e) => {
+
+        // Upon submit create the project and add comps if needed
+        // This will also cause the browser to download the project folder
+
+        // Prevent the default form submission
+        e.preventDefault()
+
+        try {
+            // Set the form to pending
+            setIsFormPending(true)
+
+            // Create the project
+            createProject()
+
+        } catch (err) {
+            console.log(err.message)
+        };
+
     };
 
 
