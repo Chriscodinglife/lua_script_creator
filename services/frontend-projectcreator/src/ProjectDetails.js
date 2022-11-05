987eca8c12bcec4d0c49e299f8d1283b4c80fdd6
Chriscodinglife <corella4@pratt.edu>
2022-11-04 23:36:40 -0400

    made use of the new modified time

--- services/frontend-projectcreator/src/ProjectDetails.js
+++ services/frontend-projectcreator/src/ProjectDetails.js
@@ -1,6 +1,7 @@
 import useFetch from "./useFetch";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import getModifiedTime from "./getModifiedTime";
 
 const ProjectDetails = () => {
 
@@ -8,6 +9,7 @@ const ProjectDetails = () => {
     const url = 'http://localhost:8000/project/' + id
     const { data: project, error, isPending } = useFetch(url)
     const navigate = useNavigate();
+
 
     const handleDelete = () => {
         fetch(url + '/delete', {
@@ -17,23 +19,45 @@ const ProjectDetails = () => {
         })
     };
 
-    const editedTimeDifference = () => {
-        const modified_date = new Date(project.modified_time);
-        const now = new Date();
 
-        const seconds = (now.getTime() - modified_date.getTime()) / 1000;
-        const hours = Math.floor(seconds / 60 /60);
-        const minutes = Math.floor(seconds /60) - (hours * 60);
+    const handleDownloadProjectFolder = () => {
+        // Download the project folder
+        let filename = "";
 
-        if (hours > 1) {
-            return (<p className="project-edit">Last edited {hours + ' hours ' + minutes + ' minutes ago'}</p>)
-        } else {
-            return (<p className="project-edit">Lasted edited {minutes + ' mins ago'}</p>)
-        }
+        const icons = false;
+        const download_url = 'http://localhost:5000/download/?project_name=' + project.project_name + '&iinclude_icons=' + icons;
+
+        fetch(download_url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw Error('Could not download project folder');
+            } else {
+                const disposition = response.headers.get('Content-Disposition');
+                filename = disposition.split(/;(.+)/)[1].split(/=(.+)/)[1];
+                if (filename.toLowerCase().startsWith("utf-8''"))
+                    filename = decodeURIComponent(filename.replace("utf-8''", ''));
+                else
+                    filename = filename.replace(/['"]/g, '');
+                return response.blob();
+            }
+        })
+        .then(blob => {
+            var url = window.URL.createObjectURL(blob); // Create a url path to download
+            var a = document.createElement('a'); // Create a link
+            a.href = url;
+            a.download = filename;
+            document.body.appendChild(a); // Append the link to the body
+            a.click();
+            window.URL.revokeObjectURL(url); // Remove the url path
+            a.remove(); // Remove the link
+        })
     };
 
     
-    const handleDownloadProject = () => {
+    const handleDownloadProjectCSV = () => {
 
         let filename = "";
 
@@ -73,9 +97,9 @@ const ProjectDetails = () => {
                     <div className="project-details-container">
                         <h1>{ project.project_name }</h1>
                         <p className="project-id">id: {project.id}</p>
-                        <h2>About: { project.project_description }</h2>
+                        <h2 className='project-desc'>About: { project.project_description }</h2>
                         <p className="project-status">Status: {project.project_status}</p>
-                        {editedTimeDifference()}
+                        {getModifiedTime(project.modified_time)}
                         {project.project_comps.Comps.map((comps, index) => (
                             <div key={index} className="project-screens">
                                 <div>Comp Name: {comps.comp_name}</div>
@@ -86,12 +110,14 @@ const ProjectDetails = () => {
                             </div>
                         ))}
                     </div>
+                    <div className="actionbuttons">
+                        <button onClick={handleDownloadProjectCSV}>Download CSV</button>
+                        <button onClick={handleDownloadProjectFolder}>Download Project Folder</button>
+                        <button className='delete' onClick={handleDelete}>Delete Project</button>
+                    </div>
                 </div>
             )}
-            <div className="actionbuttons">
-                <button onClick={handleDownloadProject}>Download CSV</button>
-                <button onClick={handleDelete}>Delete Project</button>
-            </div>
+            
         </div>
         
      );
